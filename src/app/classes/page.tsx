a39df0c244c2710af8a66ffe62b4bb03ee6fecd8
Dhakshina Deshan Siriwardhana <deshandhakshitha16@gmail.com>
2026-09-18 22:24:17 +0530

    Update page.tsx

--- src/app/classes/page.tsx
+++ src/app/classes/page.tsx
@@ -1,30 +1,8 @@
-"use client";
-
-import { useEffect, useState } from "react";
 import ClassSchedule from "@/components/ClassSchedule";
-import { auth } from "@/lib/firebase";
-import { onAuthStateChanged } from "firebase/auth";
-import { useRouter } from "next/navigation";
 
 export default function ClassesPage() {
-  const [loading, setLoading] = useState(true);
-  const router = useRouter();
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (!user) {
-        router.push("/login");
-      } else {
-        setLoading(false);
-      }
-    });
-    return () => unsubscribe();
-  }, [router]);
-
-  if (loading) return <div className="bg-academy-black min-h-screen"></div>;
-
   return (
-    <div className="pt-24 bg-academy-black min-h-screen">
+    <div className="pt-24 bg-[#07030e] min-h-screen">
       <ClassSchedule />
     </div>
   );
